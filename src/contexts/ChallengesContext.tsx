fae6f1ffa522c2cbd73cc602f787d7f06ff50eda
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-02-25 15:10:59 -0300

    Notificação: áudio de noficiação

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -50,6 +50,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     
     setActiveChallenge(challenge)
 
+
+    // notificações
+    new Audio('/notification.mp3').play();
+
     if (Notification.permission === 'granted') {
       new Notification('Novo desafio 🎉', { 
         body: `Valendo ${challenge.amount} xp!`
