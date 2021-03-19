a288e896ab95d9ca0dbc85666d8f9974e52179b6
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-03-18 23:55:36 -0300

    deixando a experience bar limpa se não está logado

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,23 +1,47 @@
 import { useContext } from 'react';
+import { useSession } from 'next-auth/client';
+
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+  const [session] = useSession();
+  const firstLevelXPToNextLevel = 144;
+
   const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
   const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
 
   return (
-    <header className={styles.experienceBar}>
-      <span>0 xp</span>
-      <div>
-        <div style={{ width: `${percentToNextLevel}%` }} />
+    <>
+      { session ? (
+        <>
+          <header className={styles.experienceBar}>
+            <span>0 xp</span>
+            <div>
+              <div style={{ width: `${percentToNextLevel}%` }} />
 
-        <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
-          {`${currentExperience} px`}
-        </span>
-      </div>
-      <span>{experienceToNextLevel} xp</span>
-    </header>
+              <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+                {`${currentExperience} px`}
+              </span>
+            </div>
+            <span>{experienceToNextLevel} xp</span>
+          </header>
+        </>
+      ) : (
+        <>
+          <header className={styles.experienceBar}>
+            <span>0 xp</span>
+            <div>
+              <span className={styles.currentExperience}>
+                0 px
+              </span>
+            </div>
+            <span>{firstLevelXPToNextLevel} xp</span>
+          </header>
+        </>
+      )
+      }
+    </>
   )
 }
