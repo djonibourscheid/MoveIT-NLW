2bee49da6f58261a70825cdeca0e4020ebf2df08
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-03-18 22:24:11 -0300

    esqueleto da página com css

---
+++ src/pages/login.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+
+import styles from '../styles/pages/Login.module.css';
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Login | move.it</title>
+      </Head>
+
+      <img src="/icons/loginPage/logo.svg" />
+
+      <section className={styles.sectionContainer}>
+        <img src="/icons/loginPage/logo-full.svg" alt="Move.it" />
+
+        <h1>Bem-vindo</h1>
+
+        <div className={styles.descriptionGithub}>
+          <img src="/icons/github.svg" alt="Logo do Github" />
+          <p>Faça login com seu Github para começar</p>
+        </div>
+      </section>
+    </div>
+  );
+}
