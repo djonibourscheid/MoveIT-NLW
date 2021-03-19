c33ac811e2f946f6ff57f4ac2ae26e0d0b3f5e6e
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-03-18 22:37:58 -0300

    Perfil do usuário igual o do github

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,21 +1,31 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { useSession } from 'next-auth/client';
 
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const [session] = useSession();
+
   const { level } = useContext(ChallengesContext);
 
   return (
-    <div className={styles.profileContainer}>
-      <img src="https://github.com/djonibourscheid.png" alt="Djoni Bourscheid" />
-      <div>
-        <strong>Djoni Bourscheid</strong>
-        <p>
-          <img src="icons/level.svg" alt="Level" />
-          Level {level}
-        </p>
-      </div>
+    <div className={styles.profileContainer} >
+      {session ? (
+        <>
+          <img src={session.user.image} alt={`Foto de ${session.user.name}`} />
+          <div className={styles.userInfos}>
+            <strong>{session.user.name}</strong>
+            <p>
+              <img src="icons/level.svg" alt="Level" />
+              Level {level}
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+        </>
+      )}
     </div>
   )
 }
